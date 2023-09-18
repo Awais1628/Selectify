f072a5be37ad686e9e28b5729a64f096dc50ed11
Awais Faheem <125728102+Awais1628@users.noreply.github.com>
2023-09-18 23:07:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+(function(){
+    const button= document.querySelectorAll('.btn')
+    const storeImage= document.querySelectorAll('.store-item')
+
+    button.forEach((button)=> {
+        button.addEventListener('click',(e) =>{
+            e.preventDefault()
+            const filter= e.target.dataset.filter
+
+            storeImage.forEach((item)=>{
+                if(filter === 'all'){
+                    item.style.display='block'
+                }
+                else{
+                    if(item.classList.contains(filter))
+                    {
+                        item.style.display='block'
+                    }
+                    else{
+                        item.style.display='none'
+                    }
+                }
+            })
+        })
+    })
+})();
